6174393c5025b85fcec100e4be8018d1d982eeab
ZempoEnrique <65579681+2212025424@users.noreply.github.com>
2022-02-17 21:18:53 -0600

    Agregue la seccion de desarrollo web

--- pages/index.jsx
+++ pages/index.jsx
@@ -12,104 +12,123 @@ export default function Index () {
                 <p className="text-content content-text-justify">Ingeniero en <span className="text-content-highlighted">Tecnologías de la información</span> por <span className="text-content-highlighted">Universidad Politécnica de Puebla</span>, el desarrollo de software es una rama de mi carrera y uno de mis pasatiempos favoritos, me agrada realizar proyectos propios y para empresas, empleando <span className="text-content-highlighted">diferentes tecnologías</span>, sin salir de las que realmente se requieren, dan <span className="text-content-highlighted">soluciones óptimas</span> y se <span className="text-content-highlighted">ajustan al presupuesto</span> presentado.</p>
             </div>
 
-            {/* A cerca del servicio */}
+            {/* Herramientas de agrado */}
 
-            <p className="text-subtitle content-my-xb">Todo servicio incluye</p>
+            <p className="text-subtitle content-my-xb">Herramientas de agrado</p>
 
-            <div className="grid-component-min-220">
+            <div className="grid-layout-content-sm">
                 <div className="simple-primary-slate">
-                    <h3 className="text-subtitle content-mb-sm">Diseño responsivo</h3>
-                    <div className="image-responsive">
-                        <Image src="/images/undraw_real_time_sync_re_nky7.svg" width="987" height="714" layout="responsive" alt="Foto de servicio de diseño resposivo" title="Foto de servicio de diseño resposivo"/>
+                    <p className="text-content content-text-center content-mb-md">html</p>
+                    <div className="image-responsive-bg">
+                        <Image src="/images/logo-html.png" width="512" height="512" layout="responsive" alt="Imágen de herramienta html" title="Imágen de herramienta html"/>
                     </div>
-                    <p className="text-content content-text-justify content-mt-sm">En todo diseño se contempla que las interfaces puedan adaptarse a las distintas medidas de los dispositivos con los que un usuario podría acceder a su sitio web.</p>
                 </div>
                 <div className="simple-primary-slate">
-                    <h3 className="text-subtitle content-mb-sm">Diseño personalizado</h3>
-                    <div className="image-responsive">
-                        <Image src="/images/undraw_building_blocks_re_rwls.svg" width="987" height="714" layout="responsive" alt="Foto de servicio de diseño personalizado" title="Foto de servicio de diseño personalizado"/>
+                    <p className="text-content content-text-center content-mb-md">css</p>
+                    <div className="image-responsive-bg">
+                        <Image src="/images/logo-css-3.png" width="512" height="512" layout="responsive" alt="Imágen de herramienta css" title="Imágen de herramienta css"/>
                     </div>
-                    <p className="text-content content-text-justify content-mt-sm">Con base en sus necesidades y criterios se determinan los diseños apropiados para su negocio o empresa, contemplando criterios de usabilidad y accesibilidad.</p>
                 </div>
                 <div className="simple-primary-slate">
-                    <h3 className="text-subtitle content-mb-sm">Optimización de SEO</h3>
-                    <div className="image-responsive">
-                        <Image src="/images/undraw_search_engines_nn-9-e.svg" width="987" height="714" layout="responsive" alt="Foto de servicio de optimización de SEO" title="Foto de servicio de optimización de SEO"/>
+                    <p className="text-content content-text-center content-mb-md">javascript</p>
+                    <div className="image-responsive-bg">
+                        <Image src="/images/logo-js.png" width="512" height="512" layout="responsive" alt="Imágen de herramienta javascript" title="Imágen de herramienta javascript"/>
                     </div>
-                    <p className="text-content content-text-justify content-mt-sm">Se contempla cada uno de los criterios de calidad actuales propuestos por la comunidad de Google, que tienen como finalidad servir contenido de forma óptima.</p>
                 </div>
                 <div className="simple-primary-slate">
-                    <h3 className="text-subtitle content-mb-sm">Código optimizado</h3>
-                    <div className="image-responsive">
-                        <Image src="/images/undraw_code_inspection_bdl7.svg" width="987" height="714" layout="responsive" alt="Foto de servicio de código optimizado" title="Foto de servicio de código optimizado"/>
+                    <p className="text-content content-text-center content-mb-md">bootstrap</p>
+                    <div className="image-responsive-bg">
+                        <Image src="/images/logo-bootstrap.png" width="512" height="512" layout="responsive" alt="Imágen de herramienta bootstrap" title="Imágen de herramienta bootstrap"/>
                     </div>
-                    <p className="text-content content-text-justify content-mt-sm">Cada una de sus solicitudes se desarrollan con las mejores prácticas de programación con la finalidad de hacer que su aplicación funcione de la manera más eficiente.</p>
                 </div>
                 <div className="simple-primary-slate">
-                    <h3 className="text-subtitle content-mb-sm">Soporte y capacitación</h3>
-                    <div className="image-responsive">
-                        <Image src="/images/undraw_youtube_tutorial_re_69qc.svg" width="987" height="714" layout="responsive" alt="Foto de servicio de soporte y capacitación" title="Foto de servicio de soporte y capacitación"/>
+                    <p className="text-content content-text-center content-mb-md">next.js</p>
+                    <div className="image-responsive-bg">
+                        <Image src="/images/logo-nextjs.png" width="512" height="512" layout="responsive" alt="Imágen de herramienta next js" title="Imágen de herramienta next js"/>
                     </div>
-                    <p className="text-content content-text-justify content-mt-sm">Una vez conluído el proyecto, se ofrece soporte y capacitación para el uso correcto del mismo, donde conocerá a la vez los términos más relevantes y empleados de la web.</p>
                 </div>
                 <div className="simple-primary-slate">
-                    <h3 className="text-subtitle content-mb-sm">Soporte SSL</h3>
-                    <div className="image-responsive">
-                        <Image src="/images/undraw_secure_login_pdn4.svg" width="987" height="714" layout="responsive" alt="Foto de servicio de soporte SSL" title="Foto de servicio de soporte SSL"/>
+                    <p className="text-content content-text-center content-mb-md">php</p>
+                    <div className="image-responsive-bg">
+                        <Image src="/images/logo-php.png" width="512" height="512" layout="responsive" alt="Imágen de herramienta php" title="Imágen de herramienta php"/>
                     </div>
-                    <p className="text-content content-text-justify content-mt-sm">En la puesta en producción se brinda un certificado de seguridad (SSL), para que así nuestra información se mantenga integra ante cualquier intersección en la red.</p>
+                </div>
+                <div className="simple-primary-slate">
+                    <p className="text-content content-text-center content-mb-md">sql</p>
+                    <div className="image-responsive-bg">
+                        <Image src="/images/logo-sql.png" width="512" height="512" layout="responsive" alt="Imágen de herramienta sql" title="Imágen de herramienta sql"/>
+                    </div>
+                </div>
+                <div className="simple-primary-slate">
+                    <p className="text-content content-text-center content-mb-md">ajax</p>
+                    <div className="image-responsive-bg">
+                        <Image src="/images/logo-ajax.png" width="512" height="512" layout="responsive" alt="Imágen de herramienta ajax" title="Imágen de herramienta ajax"/>
+                    </div>
+                </div>
+                <div className="simple-primary-slate">
+                    <p className="text-content content-text-center content-mb-md">github</p>
+                    <div className="image-responsive-bg">
+                        <Image src="/images/logo-github.png" width="512" height="512" layout="responsive" alt="Imágen de herramienta github" title="Imágen de herramienta github"/>
+                    </div>
+                </div>
+                <div className="simple-primary-slate">
+                    <p className="text-content content-text-center content-mb-md">VS Code</p>
+                    <div className="image-responsive-bg">
+                        <Image src="/images/logo-visual-studio.png" width="512" height="512" layout="responsive" alt="Imágen de herramienta visual studio code" title="Imágen de herramienta visual studio code"/>
+                    </div>
                 </div>
             </div>
+            
+            {/* Linea de tiempo, escuela y trabajo */}
 
-            {/* Principales paquetes de conveniencia */}
-        
-            <p className="text-subtitle content-my-xb">Paquetes de desarrollo</p>
+            <div className="grid-layout-content-md content-mt-xb">
+                <div>
+                    <p className="text-subtitle content-mb-xb">Grados de escolaridad</p>
 
-            <div className="grid-component-min-220">
-                <div className="simple-primary-slate content-text-center">
-                    <h3 className="text-subtitle">Paquete básico</h3>
-                    <p className="text-description content-mt-sm">Ideal para negocios con la finalidad de dar a conocer su marca</p>
-                    <h4 className="text-primary content-my-md">$ 5,000</h4>
-                    <p className="text-content content-mb-xm">Hasta 4 secciones estáticas</p>
-                    <p className="text-content content-mb-xm">Hosting eficiente por un año</p>
-                    <p className="text-content content-mb-xm">Dominio por un año</p>
-                    <p className="text-content content-mb-xm">Enlace a redes sociales</p>
-                    <p className="text-content content-mb-xm">Funcionalidad de compartir sitio</p>
-                    <p className="text-description content-mb-xm content-text-through">Boton de llamada directa</p>
-                    <p className="text-description content-mb-xm content-text-through">Galería de imágenes</p>
-                    <p className="text-description content-mb-xm content-text-through">Mapa de ubicación con google</p>
-                    <p className="text-description content-mb-bg content-text-through">Formulario de contacto</p>
-                    <div className="primary-button">Solicitar plan ahora</div>
+                    <div className="simple-primary-slate content-mb-xb">
+                        <h3 className="text-subtitle content-mb-sm">UPPuebla</h3>
+                        <p className="text-description content-text-justify content-mt-sm">Ingeniería, Tecnologías de la información</p>
+                        <p className="text-content content-text-justify content-mt-sm">Adquirí conocimientos en lenguajes como JAVA, C, PHP, HTML, CSS y SQL, bases para el manejo de Laravel, MySQL, VSCode y GitHub. Realicé prácticas en redes y de mantenimiento preventivo y correctivo.</p>
+                        <p className="text-description content-text-right content-mt-sm">[Sep. 2018 - Dic. 2021]</p>
+                    </div>
+
+                    <div className="simple-primary-slate content-mb-xb">
+                        <h3 className="text-subtitle content-mb-sm">Centro de Estudios Tecnológicos, Industrial y de Servicio</h3>
+                        <p className="text-description content-text-justify content-mt-sm">Técnico, Programación</p>
+                        <p className="text-content content-text-justify content-mt-sm">Aprendí a utilizar lo básico de WordPress, realicé mis primeras prácticas de desarrollo web básico. Manejo de base de datos con Microsoft access y terminología de redes y base de datos.</p>
+                        <p className="text-description content-text-right content-mt-sm">[Ago. 2015 - Jul. 2018]</p>
+                    </div>
+
+                    <div className="simple-primary-slate">
+                        <h3 className="text-subtitle content-mb-sm">ÁBACO Computadoras</h3>
+                        <p className="text-description content-text-justify content-mt-sm">Curso, Computación</p>
+                        <p className="text-content content-text-justify content-mt-sm">Aprendí a hacer uso de las herramientas propias del sistema operativo Windows y herramientas ofimáticas.</p>
+                        <p className="text-description content-text-right content-mt-sm">[May. 2015 - Ago. 2015]</p>
+                    </div>
                 </div>
-                <div className="simple-primary-slate content-text-center">
-                    <h3 className="text-subtitle">Paquete startup</h3>
-                    <p className="text-description content-mt-sm">Ideal para empresas con finalidad de dar a conocer sus servicios</p>
-                    <h4 className="text-primary content-my-md">$ 12,500</h4>
-                    <p className="text-content content-mb-xm">Hasta 10 secciones estáticas</p>
-                    <p className="text-content content-mb-xm">Hosting eficiente por un año</p>
-                    <p className="text-content content-mb-xm">Dominio por un año</p>
-                    <p className="text-content content-mb-xm">Enlace a redes sociales</p>
-                    <p className="text-content content-mb-xm">Funcionalidad de compartir sitio</p>
-                    <p className="text-content content-mb-xm">Boton de llamada directa</p>
-                    <p className="text-content content-mb-xm">Galería de imágenes</p>
-                    <p className="text-content content-mb-xm">Mapa de ubicación con google</p>
-                    <p className="text-content content-mb-bg">Formulario de contacto</p>
-                    <div className="primary-button">Solicitar plan ahora</div>
-                </div>
-                <div className="simple-primary-slate content-text-center">
-                    <h3 className="text-subtitle">Paquete tienda en línea</h3>
-                    <p className="text-description content-mt-sm">Ideal para negocios con productos o servicios variables</p>
-                    <h4 className="text-primary content-my-md">$ 15,000</h4>
-                    <p className="text-content content-mb-xm">Hasta 3 secciones estáticas</p>
-                    <p className="text-content content-mb-xm">Hasta 3 secciones administrables</p>
-                    <p className="text-content content-mb-xm">Pasarela de pago en linea</p>
-                    <p className="text-content content-mb-xm">Hosting eficiente por un año</p>
-                    <p className="text-content content-mb-xm">Dominio por un año</p>
-                    <p className="text-content content-mb-xm">Enlace a redes sociales</p>
-                    <p className="text-content content-mb-xm">Galería de imágenes</p>
-                    <p className="text-content content-mb-xm">Mapa de ubicación con google</p>
-                    <p className="text-content content-mb-bg">Formulario de contacto</p>
-                    <div className="primary-button">Solicitar plan ahora</div>
+                <div>
+                    <p className="text-subtitle content-mb-xb">Historial de trabajo</p>
+                    
+                    <div className="simple-primary-slate content-mb-xb">
+                        <h3 className="text-subtitle content-mb-sm">Desarrollador web</h3>
+                        <p className="text-description content-mb-xm">Consultoría contracorriente S.A de C.V</p>
+                        <p className="text-content content-text-justify content-mt-sm">Mi actual trabajo después de realizar mis prácticas profesionales desarrollando una aplicación en Next.js para el periódico <a href="https://e-consulta.com/" target="_blank">e-consulta.com</a>, misma que detalla contenido y redirige tráfico dependiendo el dispositivo.</p>
+                        <p className="text-description content-text-right content-mt-sm">[Ago. 2021 - Actulmente]</p>
+                    </div>
+                    
+                    <div className="simple-primary-slate content-mb-xb">
+                        <h3 className="text-subtitle content-mb-sm">Técnico en computación</h3>
+                        <p className="text-description content-mb-xm">H. Ayuntamiento de Coronango Puebla</p>
+                        <p className="text-content content-text-justify content-mt-sm">Las actividades que realicé fueron dar mantenimiento preventivo y correctivo a los equipos de cómputo, a la vez resolver dudas con el manejo de herramientas de sistema y de aplicación (Servicio social – UPPuebla).</p>
+                        <p className="text-description content-text-right content-mt-sm">[Feb. 2021 - Ago. 2021]</p>
+                    </div>
+                    
+                    <div className="simple-primary-slate">
+                        <h3 className="text-subtitle content-mb-sm">Profesor en desarrollo de web y edición de imágenes</h3>
+                        <p className="text-description content-mb-xm">BUSPEC, San Pedro Cholula</p>
+                        <p className="text-content content-text-justify content-mt-sm">Apoyé impartiendo dos cursos, uno orientado al desarrollo web básico con herramientas HTML Y CSS, el segundo requería guiar el manejo de herramientas que permiten la edición de imágenes como photoshop.</p>
+                        <p className="text-description content-text-right content-mt-sm">[Ene. 2020 - Abr. 2020]</p>
+                    </div>
                 </div>
             </div>
         </div>
