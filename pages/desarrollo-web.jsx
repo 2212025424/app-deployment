a245202f4eee443c0adb91d14f5e2434becd82b4
ZempoEnrique <65579681+2212025424@users.noreply.github.com>
2022-02-19 20:06:52 -0600

    Arreglando contenido de paquetes de desarrollo

--- pages/desarrollo-web.jsx
+++ pages/desarrollo-web.jsx
@@ -6,46 +6,46 @@ export default function Index () {
 
             {/* A cerca del servicio */}
 
-            <p className="text-subtitle content-mb-xb">Todo servicio incluye</p>
+            <p className="text-subtitle-primary content-mb-xb">Todos los paquetes incluyen</p>
 
             <div className="grid-layout-content-md">
                 <div className="simple-primary-slate">
-                    <h3 className="text-subtitle content-mb-sm">Diseño responsivo</h3>
+                    <h3 className="text-subtitle-secondary content-mb-sm">Diseño responsivo</h3>
                     <div className="image-responsive-md">
                         <Image src="/images/undraw_real_time_sync_re_nky7.svg" width="987" height="714" layout="responsive" alt="Foto de servicio de diseño resposivo" title="Foto de servicio de diseño resposivo"/>
                     </div>
                     <p className="text-content content-text-justify content-mt-sm">En todo diseño se contempla que las interfaces puedan adaptarse a las distintas medidas de los dispositivos con los que un usuario podría acceder a su sitio web.</p>
                 </div>
                 <div className="simple-primary-slate">
-                    <h3 className="text-subtitle content-mb-sm">Diseño personalizado</h3>
+                    <h3 className="text-subtitle-secondary content-mb-sm">Diseño personalizado</h3>
                     <div className="image-responsive-md">
                         <Image src="/images/undraw_building_blocks_re_rwls.svg" width="987" height="714" layout="responsive" alt="Foto de servicio de diseño personalizado" title="Foto de servicio de diseño personalizado"/>
                     </div>
                     <p className="text-content content-text-justify content-mt-sm">Con base en sus necesidades y criterios se determinan los diseños apropiados para su negocio o empresa, contemplando criterios de usabilidad y accesibilidad.</p>
                 </div>
                 <div className="simple-primary-slate">
-                    <h3 className="text-subtitle content-mb-sm">Optimización de SEO</h3>
+                    <h3 className="text-subtitle-secondary content-mb-sm">Optimización de SEO</h3>
                     <div className="image-responsive-md">
                         <Image src="/images/undraw_search_engines_nn-9-e.svg" width="987" height="714" layout="responsive" alt="Foto de servicio de optimización de SEO" title="Foto de servicio de optimización de SEO"/>
                     </div>
                     <p className="text-content content-text-justify content-mt-sm">Se contempla cada uno de los criterios de calidad actuales propuestos por la comunidad de Google, que tienen como finalidad servir contenido de forma óptima.</p>
                 </div>
                 <div className="simple-primary-slate">
-                    <h3 className="text-subtitle content-mb-sm">Código optimizado</h3>
+                    <h3 className="text-subtitle-secondary content-mb-sm">Código optimizado</h3>
                     <div className="image-responsive-md">
                         <Image src="/images/undraw_code_inspection_bdl7.svg" width="987" height="714" layout="responsive" alt="Foto de servicio de código optimizado" title="Foto de servicio de código optimizado"/>
                     </div>
                     <p className="text-content content-text-justify content-mt-sm">Cada una de sus solicitudes se desarrollan con las mejores prácticas de programación con la finalidad de hacer que su aplicación funcione de la manera más eficiente.</p>
                 </div>
                 <div className="simple-primary-slate">
-                    <h3 className="text-subtitle content-mb-sm">Soporte y capacitación</h3>
+                    <h3 className="text-subtitle-secondary content-mb-sm">Soporte y capacitación</h3>
                     <div className="image-responsive-md">
                         <Image src="/images/undraw_youtube_tutorial_re_69qc.svg" width="987" height="714" layout="responsive" alt="Foto de servicio de soporte y capacitación" title="Foto de servicio de soporte y capacitación"/>
                     </div>
                     <p className="text-content content-text-justify content-mt-sm">Una vez conluído el proyecto, se ofrece soporte y capacitación para el uso correcto del mismo, donde conocerá a la vez los términos más relevantes y empleados de la web.</p>
                 </div>
                 <div className="simple-primary-slate">
-                    <h3 className="text-subtitle content-mb-sm">Soporte SSL</h3>
+                    <h3 className="text-subtitle-secondary content-mb-sm">Soporte SSL</h3>
                     <div className="image-responsive-md">
                         <Image src="/images/undraw_secure_login_pdn4.svg" width="987" height="714" layout="responsive" alt="Foto de servicio de soporte SSL" title="Foto de servicio de soporte SSL"/>
                     </div>
@@ -55,12 +55,12 @@ export default function Index () {
 
             {/* Principales paquetes de conveniencia */}
         
-            <p className="text-subtitle content-my-xb">Paquetes de desarrollo</p>
+            <p className="text-subtitle-primary content-my-xb">Paquetes de desarrollo</p>
 
             <div className="grid-layout-content-md">
                 <div className="simple-primary-slate content-text-center">
-                    <h3 className="text-subtitle">Paquete básico</h3>
-                    <p className="text-description content-mt-sm">Ideal para negocios con la finalidad de dar a conocer su marca</p>
+                    <h3 className="text-subtitle-secondary">Sitio básico</h3>
+                    <p className="text-description content-mt-sm">Ideal cuando una empresa o negocio requiere comenzar a tener presencia en internet</p>
                     <h4 className="text-primary content-my-md">$ 5,000</h4>
                     <p className="text-content content-mb-xm">Hasta 4 secciones estáticas</p>
                     <p className="text-content content-mb-xm">Hosting eficiente por un año</p>
@@ -71,11 +71,26 @@ export default function Index () {
                     <p className="text-description content-mb-xm content-text-through">Galería de imágenes</p>
                     <p className="text-description content-mb-xm content-text-through">Mapa de ubicación con google</p>
                     <p className="text-description content-mb-bg content-text-through">Formulario de contacto</p>
-                    <div className="primary-button">Solicitar plan ahora</div>
+                    <div className="primary-button">Ver detalles</div>
                 </div>
                 <div className="simple-primary-slate content-text-center">
-                    <h3 className="text-subtitle">Paquete startup</h3>
-                    <p className="text-description content-mt-sm">Ideal para empresas con finalidad de dar a conocer sus servicios</p>
+                    <h3 className="text-subtitle-secondary">Sitio empresarial</h3>
+                    <p className="text-description content-mt-sm">Ideal para dar a conocer su empresa de manera profesional y ofertar productos o servicios</p>
+                    <h4 className="text-primary content-my-md">$ 5,000</h4>
+                    <p className="text-content content-mb-xm">Hasta 4 secciones estáticas</p>
+                    <p className="text-content content-mb-xm">Hosting eficiente por un año</p>
+                    <p className="text-content content-mb-xm">Dominio por un año</p>
+                    <p className="text-content content-mb-xm">Enlace a redes sociales</p>
+                    <p className="text-content content-mb-xm">Funcionalidad de compartir sitio</p>
+                    <p className="text-description content-mb-xm content-text-through">Boton de llamada directa</p>
+                    <p className="text-description content-mb-xm content-text-through">Galería de imágenes</p>
+                    <p className="text-description content-mb-xm content-text-through">Mapa de ubicación con google</p>
+                    <p className="text-description content-mb-bg content-text-through">Formulario de contacto</p>
+                    <div className="primary-button">Ver detalles</div>
+                </div>
+                <div className="simple-primary-slate content-text-center">
+                    <h3 className="text-subtitle-secondary">Gestor de contenido</h3>
+                    <p className="text-description content-mt-sm">Ideal para empresas o negocios que requieren subir, modificar o eliminar contenido de forma eventual</p>
                     <h4 className="text-primary content-my-md">$ 12,500</h4>
                     <p className="text-content content-mb-xm">Hasta 10 secciones estáticas</p>
                     <p className="text-content content-mb-xm">Hosting eficiente por un año</p>
@@ -86,11 +101,11 @@ export default function Index () {
                     <p className="text-content content-mb-xm">Galería de imágenes</p>
                     <p className="text-content content-mb-xm">Mapa de ubicación con google</p>
                     <p className="text-content content-mb-bg">Formulario de contacto</p>
-                    <div className="primary-button">Solicitar plan ahora</div>
+                    <div className="primary-button">Ver detalles</div>
                 </div>
                 <div className="simple-primary-slate content-text-center">
-                    <h3 className="text-subtitle">Paquete tienda en línea</h3>
-                    <p className="text-description content-mt-sm">Ideal para negocios con productos o servicios variables</p>
+                    <h3 className="text-subtitle-secondary">Productos o servicios en linea</h3>
+                    <p className="text-description content-mt-sm">Ideal para negocios que requieren ofertar y gestionar sus productos o servicios de manera eficiente</p>
                     <h4 className="text-primary content-my-md">$ 15,000</h4>
                     <p className="text-content content-mb-xm">Hasta 3 secciones estáticas</p>
                     <p className="text-content content-mb-xm">Hasta 3 secciones administrables</p>
@@ -101,7 +116,37 @@ export default function Index () {
                     <p className="text-content content-mb-xm">Galería de imágenes</p>
                     <p className="text-content content-mb-xm">Mapa de ubicación con google</p>
                     <p className="text-content content-mb-bg">Formulario de contacto</p>
-                    <div className="primary-button">Solicitar plan ahora</div>
+                    <div className="primary-button">Ver detalles</div>
+                </div>
+                <div className="simple-primary-slate content-text-center">
+                    <h3 className="text-subtitle-secondary">Tienda en línea</h3>
+                    <p className="text-description content-mt-sm">Ideal para negocios que requieren ofertar, gestionar y realizar ventas en linea de sus productos o servicios</p>
+                    <h4 className="text-primary content-my-md">$ 15,000</h4>
+                    <p className="text-content content-mb-xm">Hasta 3 secciones estáticas</p>
+                    <p className="text-content content-mb-xm">Hasta 3 secciones administrables</p>
+                    <p className="text-content content-mb-xm">Pasarela de pago en linea</p>
+                    <p className="text-content content-mb-xm">Hosting eficiente por un año</p>
+                    <p className="text-content content-mb-xm">Dominio por un año</p>
+                    <p className="text-content content-mb-xm">Enlace a redes sociales</p>
+                    <p className="text-content content-mb-xm">Galería de imágenes</p>
+                    <p className="text-content content-mb-xm">Mapa de ubicación con google</p>
+                    <p className="text-content content-mb-bg">Formulario de contacto</p>
+                    <div className="primary-button">Ver detalles</div>
+                </div>
+                <div className="simple-primary-slate content-text-center">
+                    <h3 className="text-subtitle-secondary">Sistema de inventario</h3>
+                    <p className="text-description content-mt-sm">Para empresa o negocio que pretende administrar sus recursos con entradas, salidas y generación de reportes</p>
+                    <h4 className="text-primary content-my-md">$ 15,000</h4>
+                    <p className="text-content content-mb-xm">Hasta 3 secciones estáticas</p>
+                    <p className="text-content content-mb-xm">Hasta 3 secciones administrables</p>
+                    <p className="text-content content-mb-xm">Pasarela de pago en linea</p>
+                    <p className="text-content content-mb-xm">Hosting eficiente por un año</p>
+                    <p className="text-content content-mb-xm">Dominio por un año</p>
+                    <p className="text-content content-mb-xm">Enlace a redes sociales</p>
+                    <p className="text-content content-mb-xm">Galería de imágenes</p>
+                    <p className="text-content content-mb-xm">Mapa de ubicación con google</p>
+                    <p className="text-content content-mb-bg">Formulario de contacto</p>
+                    <div className="primary-button">Ver detalles</div>
                 </div>
             </div>
         </div>
--- pages/index.jsx
+++ pages/index.jsx
@@ -14,7 +14,7 @@ export default function Index () {
 
             {/* Herramientas de agrado */}
 
-            <p className="text-subtitle content-my-xb">Herramientas de agrado</p>
+            <p className="text-subtitle-primary content-my-xb">Herramientas de agrado</p>
 
             <div className="grid-layout-content-sm">
                 <div className="simple-primary-slate">
@@ -83,48 +83,48 @@ export default function Index () {
 
             <div className="grid-layout-content-md content-mt-xb">
                 <div>
-                    <p className="text-subtitle content-mb-xb">Grados de escolaridad</p>
+                    <p className="text-subtitle-primary content-mb-xb">Grados de escolaridad</p>
 
                     <div className="simple-primary-slate content-mb-xb">
-                        <h3 className="text-subtitle content-mb-sm">UPPuebla</h3>
+                        <h3 className="text-subtitle-secondary content-mb-sm">UPPuebla</h3>
                         <p className="text-description content-text-justify content-mt-sm">Ingeniería, Tecnologías de la información</p>
                         <p className="text-content content-text-justify content-mt-sm">Adquirí conocimientos en lenguajes como JAVA, C, PHP, HTML, CSS y SQL, bases para el manejo de Laravel, MySQL, VSCode y GitHub. Realicé prácticas en redes y de mantenimiento preventivo y correctivo.</p>
                         <p className="text-description content-text-right content-mt-sm">[Sep. 2018 - Dic. 2021]</p>
                     </div>
 
                     <div className="simple-primary-slate content-mb-xb">
-                        <h3 className="text-subtitle content-mb-sm">Centro de Estudios Tecnológicos, Industrial y de Servicio</h3>
+                        <h3 className="text-subtitle-secondary content-mb-sm">Centro de Estudios Tecnológicos, Industrial y de Servicio</h3>
                         <p className="text-description content-text-justify content-mt-sm">Técnico, Programación</p>
                         <p className="text-content content-text-justify content-mt-sm">Aprendí a utilizar lo básico de WordPress, realicé mis primeras prácticas de desarrollo web básico. Manejo de base de datos con Microsoft access y terminología de redes y base de datos.</p>
                         <p className="text-description content-text-right content-mt-sm">[Ago. 2015 - Jul. 2018]</p>
                     </div>
 
                     <div className="simple-primary-slate">
-                        <h3 className="text-subtitle content-mb-sm">ÁBACO Computadoras</h3>
+                        <h3 className="text-subtitle-secondary content-mb-sm">ÁBACO Computadoras</h3>
                         <p className="text-description content-text-justify content-mt-sm">Curso, Computación</p>
                         <p className="text-content content-text-justify content-mt-sm">Aprendí a hacer uso de las herramientas propias del sistema operativo Windows y herramientas ofimáticas.</p>
                         <p className="text-description content-text-right content-mt-sm">[May. 2015 - Ago. 2015]</p>
                     </div>
                 </div>
                 <div>
-                    <p className="text-subtitle content-mb-xb">Historial de trabajo</p>
+                    <p className="text-subtitle-primary content-mb-xb">Historial de trabajo</p>
                     
                     <div className="simple-primary-slate content-mb-xb">
-                        <h3 className="text-subtitle content-mb-sm">Desarrollador web</h3>
+                        <h3 className="text-subtitle-secondary content-mb-sm">Desarrollador web</h3>
                         <p className="text-description content-mb-xm">Consultoría contracorriente S.A de C.V</p>
                         <p className="text-content content-text-justify content-mt-sm">Mi actual trabajo después de realizar mis prácticas profesionales desarrollando una aplicación en Next.js para el periódico <a href="https://e-consulta.com/" target="_blank">e-consulta.com</a>, misma que detalla contenido y redirige tráfico dependiendo el dispositivo.</p>
                         <p className="text-description content-text-right content-mt-sm">[Ago. 2021 - Actulmente]</p>
                     </div>
                     
                     <div className="simple-primary-slate content-mb-xb">
-                        <h3 className="text-subtitle content-mb-sm">Técnico en computación</h3>
+                        <h3 className="text-subtitle-secondary content-mb-sm">Técnico en computación</h3>
                         <p className="text-description content-mb-xm">H. Ayuntamiento de Coronango Puebla</p>
                         <p className="text-content content-text-justify content-mt-sm">Las actividades que realicé fueron dar mantenimiento preventivo y correctivo a los equipos de cómputo, a la vez resolver dudas con el manejo de herramientas de sistema y de aplicación (Servicio social – UPPuebla).</p>
                         <p className="text-description content-text-right content-mt-sm">[Feb. 2021 - Ago. 2021]</p>
                     </div>
                     
                     <div className="simple-primary-slate">
-                        <h3 className="text-subtitle content-mb-sm">Profesor en desarrollo de web y edición de imágenes</h3>
+                        <h3 className="text-subtitle-secondary content-mb-sm">Profesor en desarrollo de web y edición de imágenes</h3>
                         <p className="text-description content-mb-xm">BUSPEC, San Pedro Cholula</p>
                         <p className="text-content content-text-justify content-mt-sm">Apoyé impartiendo dos cursos, uno orientado al desarrollo web básico con herramientas HTML Y CSS, el segundo requería guiar el manejo de herramientas que permiten la edición de imágenes como photoshop.</p>
                         <p className="text-description content-text-right content-mt-sm">[Ene. 2020 - Abr. 2020]</p>
